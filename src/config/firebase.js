5b7e0120890c57e1ca5752605b0551f4906e1b32
mohit25T <158540458+mohit25T@users.noreply.github.com>
2026-02-08 22:03:11 +0530

    Update firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,15 +1,15 @@
 import admin from "firebase-admin";
 
 if (!process.env.FIREBASE_SERVICE_ACCOUNT) {
-  throw new Error("FIREBASE_SERVICE_ACCOUNT is not set");
+    throw new Error("FIREBASE_SERVICE_ACCOUNT is not set");
 }
 
 const serviceAccount = JSON.parse(
-  process.env.FIREBASE_SERVICE_ACCOUNT
+    process.env.FIREBASE_SERVICE_ACCOUNT
 );
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+    credential: admin.credential.cert(serviceAccount),
 });
 
 export default admin;
