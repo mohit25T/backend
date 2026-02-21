638bbd67a30d3c0870bb87deb911738c2fc25b3f
mohit25T <158540458+mohit25T@users.noreply.github.com>
2026-02-21 19:12:19 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -24,7 +24,7 @@ app.use((req, res, next) => {
   next();
 });
 
-//app.use(cors());
+app.use(cors());
 app.use(express.json());
 app.use(cors({
   origin: "https://web-deploy-j4qo.onrender.com", // or your domain
