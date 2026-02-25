5e73200979c44d78cc43548e9fe30f7d52e745b9
mohit25T <mohittopiya9106@gmail.com>
2026-02-25 21:57:32 +0530

    555

--- src/middlewares/upload.middleware.js
+++ src/middlewares/upload.middleware.js
@@ -31,7 +31,7 @@ const fileFilter = (req, file, cb) => {
 const upload = multer({
   storage,
   fileFilter,
-  limits: { fileSize: 2 * 1024 * 1024 }
+  limits: { fileSize: 20 * 1024 * 1024 }
 });
 
 
